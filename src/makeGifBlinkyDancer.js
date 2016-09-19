747225c98402503f0b5ba32bc670510c9b2b0de5
Albosonic <albertomadueno@gmail.com>
2016-09-19 16:01:52 -0700

    add assets folder with several potential images for dancers. Get gifBlinkyDancer working with the polar bear image.

--- src/makeGifBlinkyDancer.js
+++ src/makeGifBlinkyDancer.js
@@ -1,6 +1,6 @@
 var makeGifBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeBlinkyDancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<span class="gifDancer"></span>');
+  this.$node = $('<image class="gifDancer" src="assets/polarBear.png"></span>');
   
   this.setPosition(top, left);
 };
